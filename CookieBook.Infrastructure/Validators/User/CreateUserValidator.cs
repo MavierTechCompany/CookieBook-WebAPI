c38de4e13f1cc47d73546ada59389c37e818838e
Michał B <cyberwolf_96@outlook.com>
2018-07-29 17:29:05 +0200

    Added regex for ulongInt.

--- CookieBook.Infrastructure/Validators/User/CreateUserValidator.cs
+++ CookieBook.Infrastructure/Validators/User/CreateUserValidator.cs
@@ -16,17 +16,20 @@ namespace CookieBook.Infrastructure.Validators.User
             RuleFor(req => req.Login)
                 .NotEmpty()
                 .MinimumLength(19)
-                .MaximumLength(20);
+                .MaximumLength(20)
+                .Matches(@"^[1-9]\d[0-9]\d+$");
 
             RuleFor(req => req.Password)
                 .NotEmpty()
                 .MinimumLength(19)
-                .MaximumLength(20);
+                .MaximumLength(20)
+                .Matches(@"^[1-9]\d[0-9]\d+$");
 
             RuleFor(req => req.UserEmail)
                 .NotEmpty()
                 .MinimumLength(19)
-                .MaximumLength(20);
+                .MaximumLength(20)
+                .Matches(@"^[1-9]\d[0-9]\d+$");
         }
     }
 }
