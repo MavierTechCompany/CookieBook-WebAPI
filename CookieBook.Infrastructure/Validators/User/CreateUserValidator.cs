ff1e453a456362ae3a6665d71c3ef28f035a5879
Michał B <cyberwolf_96@outlook.com>
2018-07-29 17:45:56 +0200

    Created controller for user management and action (not implemented)

--- CookieBook.Infrastructure/Validators/User/CreateUserValidator.cs
+++ CookieBook.Infrastructure/Validators/User/CreateUserValidator.cs
@@ -3,33 +3,33 @@ using FluentValidation;
 
 namespace CookieBook.Infrastructure.Validators.User
 {
-    public class CreateUserValidator : AbstractValidator<CreateUser>
-    {
-        public CreateUserValidator()
-        {
-            RuleFor(req => req.Nick)
-                .NotEmpty()
-                .MinimumLength(3)
-                .MaximumLength(20)
-                .Matches(@"^[A-Za-z][A-Za-z0-9_-]+$");
+	public class CreateUserValidator : AbstractValidator<CreateUser>
+	{
+		public CreateUserValidator()
+		{
+			RuleFor(req => req.Nick)
+				.NotEmpty()
+				.MinimumLength(3)
+				.MaximumLength(20)
+				.Matches(@"^[A-Za-z][A-Za-z0-9_-]+$");
 
-            RuleFor(req => req.Login)
-                .NotEmpty()
-                .MinimumLength(19)
-                .MaximumLength(20)
-                .Matches(@"^[1-9]\d[0-9]\d+$");
+			RuleFor(req => req.Login)
+				.NotEmpty()
+				.MinimumLength(19)
+				.MaximumLength(20)
+				.Matches(@"^[1-9]\d[0-9]\d+$");
 
-            RuleFor(req => req.Password)
-                .NotEmpty()
-                .MinimumLength(19)
-                .MaximumLength(20)
-                .Matches(@"^[1-9]\d[0-9]\d+$");
+			RuleFor(req => req.Password)
+				.NotEmpty()
+				.MinimumLength(19)
+				.MaximumLength(20)
+				.Matches(@"^[1-9]\d[0-9]\d+$");
 
-            RuleFor(req => req.UserEmail)
-                .NotEmpty()
-                .MinimumLength(19)
-                .MaximumLength(20)
-                .Matches(@"^[1-9]\d[0-9]\d+$");
-        }
-    }
+			RuleFor(req => req.UserEmail)
+				.NotEmpty()
+				.MinimumLength(19)
+				.MaximumLength(20)
+				.Matches(@"^[1-9]\d[0-9]\d+$");
+		}
+	}
 }
---
+++ CookieBook.WebAPI/Controllers/UserManagementController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using CookieBook.Infrastructure.Commands.User;
+using CookieBook.Infrastructure.Services.Interfaces;
+using CookieBook.WebAPI.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookieBook.WebAPI.Controllers
+{
+    [Route("user-management")]
+    public class UserManagementController : ApiControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserManagementController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("users")]
+        public async Task<IActionResult> CreateUserAsync([FromBody] CreateUser command)
+        {
+            try
+            {
+                await _userService.AddUserAsync(command);
+                return StatusCode(201); //Should use Creater(uri, value) instead
+            }
+            catch (System.Exception ex)
+            {
+                return Json(ex.Message);
+            }
+        }
+    }
+}
