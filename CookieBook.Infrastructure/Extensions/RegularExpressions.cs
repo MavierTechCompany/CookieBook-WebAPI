2320061cf8972dc55895b4938abe1a8ddd4cc147
Michał B <cyberwolf_96@outlook.com>
2018-08-03 23:32:52 +0200

    little fix

--- CookieBook.Infrastructure/Extensions/RegularExpressions.cs
+++ CookieBook.Infrastructure/Extensions/RegularExpressions.cs
@@ -3,6 +3,5 @@ namespace CookieBook.Infrastructure.Extensions
     public static class RegularExpressions
     {
         public static string Nick { get; } = @"^[A-Za-z][A-Za-z0-9_-]+$";
-
     }
 }
