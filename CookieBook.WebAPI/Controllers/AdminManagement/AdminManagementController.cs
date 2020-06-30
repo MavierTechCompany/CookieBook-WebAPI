06e203e6a97c5aa5515d7d6f421e19379d088fa2
Michał B <MadTiger2409@users.noreply.github.com>
2020-06-30 14:56:14 +0200

    Working on admin controller. I should move IMapper variable to the ApiControllerBase because I am using it most of the time :)

---
+++ CookieBook.WebAPI/Controllers/AdminManagement/AdminManagementController.cs
@@ -0,0 +1,25 @@
+﻿using CookieBook.WebAPI.Controllers.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieBook.WebAPI.Controllers.AdminManagement
+{
+    [Route("admin-management/admins")]
+    public class AdminManagementController : ApiControllerBase
+    {
+        [HttpPost("token")]
+        public async Task<IActionResult> LoginAdminAsync() => throw new NotImplementedException();
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> CreateAdminAsync() => throw new NotImplementedException();
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> UpdateAdminAsync() => throw new NotImplementedException();
+    }
+}
