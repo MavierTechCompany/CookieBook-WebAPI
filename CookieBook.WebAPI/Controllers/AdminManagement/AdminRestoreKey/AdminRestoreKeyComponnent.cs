fa3cc3053bee65eddc45011ded1c57ddd596d494
Michał B <MadTiger2409@users.noreply.github.com>
2020-07-22 21:13:41 +0200

    Created endpoints. Next I need to create commands classes for incoming data

---
+++ CookieBook.WebAPI/Controllers/AdminManagement/AdminRestoreKey/AdminRestoreKeyComponnent.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CookieBook.WebAPI.Controllers.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieBook.WebAPI.Controllers.AdminManagement.AdminRestoreKey
+{
+    [Route("admin-management/admins/")]
+    public class AdminRestoreKeyComponnent : ApiControllerBase
+    {
+        public AdminRestoreKeyComponnent(IMapper mapper) : base(mapper)
+        {
+        }
+
+        [HttpPost("block")]
+        public async Task<IActionResult> BlockAdminAsync() => throw new NotImplementedException();
+
+        [HttpPost("request-unblock")]
+        public async Task<IActionResult> RequestUnblockAdminAsync() => throw new NotImplementedException();
+
+        [Authorize("admin")]
+        [HttpPost("{id}/unblock")]
+        public async Task<IActionResult> UnblockSelectedAdminAsync(int id) => throw new NotImplementedException();
+
+        [Authorize("admin")]
+        [HttpPost("{id}/restore-key")]
+        public async Task<IActionResult> GenerateRestoreKeyAsync(int id) => throw new NotImplementedException();
+    }
+}
---
+++ CookieBook.WebAPI/Controllers/UserManagement/UserRestoreKey/UserRestoreKeyController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CookieBook.WebAPI.Controllers.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieBook.WebAPI.Controllers.UserManagement.UserRestoreKey
+{
+    [Route("user-management/users/")]
+    public class UserRestoreKeyController : ApiControllerBase
+    {
+        public UserRestoreKeyController(IMapper mapper) : base(mapper)
+        {
+        }
+
+        [HttpPost("block")]
+        public async Task<IActionResult> BlockUserAsync() => throw new NotImplementedException();
+
+        [HttpPost("request-unblock")]
+        public async Task<IActionResult> RequestUnblockUserAsync() => throw new NotImplementedException();
+
+        [Authorize("user")]
+        [HttpPost("{id}/restore-key")]
+        public async Task<IActionResult> GenerateRestoreKeyAsync(int id) => throw new NotImplementedException();
+    }
+}
