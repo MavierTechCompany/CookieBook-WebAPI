5b07adf4df941afe127de3b97560cf521236312e
Michał B <MadTiger2409@users.noreply.github.com>
2020-10-13 18:17:46 +0200

    Need implementation

---
+++ CookieBook.WebAPI/Filters/AccessableByInactiveAccountResourceFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieBook.WebAPI.Filters
+{
+    public class AccessableByInactiveAccountResourceFilter : IAsyncResourceFilter
+    {
+        public Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            //TODO Implement code from access middleware and ad a bool parameter
+            throw new NotImplementedException();
+        }
+    }
+}
