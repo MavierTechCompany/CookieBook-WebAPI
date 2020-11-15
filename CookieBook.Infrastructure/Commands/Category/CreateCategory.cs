ac84947d1f13a9a13c8e7f6fe0268750e8189ad1
Michał B <MadTiger2409@users.noreply.github.com>
2020-11-15 22:44:47 +0100

    Another part of documentation comments

--- CookieBook.Infrastructure/Commands/Category/CreateCategory.cs
+++ CookieBook.Infrastructure/Commands/Category/CreateCategory.cs
@@ -1,11 +1,22 @@
+using CookieBook.Infrastructure.Extensions;
+using System.ComponentModel.DataAnnotations;
+
 namespace CookieBook.Infrastructure.Commands.Category
 {
+    /// <summary>
+    /// Represents a set of data used to create a category.
+    /// </summary>
     public class CreateCategory
     {
         /// <summary>
         /// A name of the category.
         /// </summary>
         /// <example>Fast Food</example>
+        ///
+        [Required]
+        [MinLength(4)]
+        [MaxLength(150)]
+        [RegularExpression(RegularExpressions.WholeSentence)]
         public string Name { get; set; }
     }
 }
--- CookieBook.WebAPI/Controllers/CategoryManagement/CategoryManagementController.cs
+++ CookieBook.WebAPI/Controllers/CategoryManagement/CategoryManagementController.cs
@@ -29,13 +29,14 @@ namespace CookieBook.WebAPI.Controllers.CategoryManagement
         /// Creates new category
         /// </summary>
         /// <param name="command"></param>
-        /// <returns>Newly category</returns>
         /// <response code="201">Returns the newly created category</response>
         /// <response code="400">Returns information about failed validation</response>
+        /// <response code="401">Returned when caller/sender doesn't have permission to do this action</response>
         [HttpPost]
         [Authorize(Roles = "admin")]
         [ProducesResponseType(typeof(CategoryDto), 201)]
         [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
         public async Task<IActionResult> CreateCategoryAsync([FromBody] CreateCategory command)
         {
             var category = await _categoryService.AddAsync(command);
@@ -44,7 +45,15 @@ namespace CookieBook.WebAPI.Controllers.CategoryManagement
             return Created($"{Request.Host}{Request.Path}/{category.Id}", categoryDto);
         }
 
+        /// <summary>
+        /// Returns collection of categories
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <response code="200">Returns collection of categories that matched given criteria. May be empty if no item matching the search criteria could be found.</response>
+        /// <response code="400">Returned when parameter <b>Fields</b> contains name of a field that isn't a part of the <b>Category</b> object.</response>
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CategoryDto>), 200)]
+        [ProducesResponseType(400)]
         public async Task<IActionResult> ReadCategoriesAsync(CategoryParameters parameters)
         {
             if (!string.IsNullOrWhiteSpace(parameters.Fields) &&
@@ -59,7 +68,16 @@ namespace CookieBook.WebAPI.Controllers.CategoryManagement
             return Ok(categoriesDto.ShapeData(parameters.Fields));
         }
 
+        /// <summary>
+        /// Returns category with the given ID
+        /// </summary>
+        /// <param name="id" example="1">Category ID</param>
+        /// <param name="fields" example="Name,Id,CreatedAt">Names of categry's fields that caller wants to get. Must contains names that are a part of the category object. Names must be separated by a comma</param>
+        /// <response code="200">Returns data on the selected category or a subset of it, as specified in the fields.</response>
+        /// <response code="400">Returned when parameter <b>Fields</b> contains name of a field that isn't a part of the <b>Category</b> object.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(CategoryDto), 200)]
+        [ProducesResponseType(400)]
         public async Task<IActionResult> ReadCategoryAsync(int id, [FromQuery] string fields)
         {
             if (!string.IsNullOrWhiteSpace(fields) &&
