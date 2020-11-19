02a53c37049f39075727e05a7d45f31710f45079
Michał B <MadTiger2409@users.noreply.github.com>
2020-11-19 12:21:06 +0100

    Changes to the login/jwt response

---
+++ CookieBook.Infrastructure/DTO/Base/JwtDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookieBook.Infrastructure.DTO.Base
+{
+    public class JwtDto
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ CookieBook.Infrastructure/Profiles/JwtProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CookieBook.Infrastructure.DTO.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookieBook.Infrastructure.Profiles
+{
+    public class JwtProfile : Profile
+    {
+        public JwtProfile()
+        {
+            CreateMap<string, JwtDto>()
+                .ForMember(dto => dto.Token, opt => opt.MapFrom(y => y));
+        }
+    }
+}
