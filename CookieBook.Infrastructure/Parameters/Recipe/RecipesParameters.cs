b96f8220f745a0e8142ee74f2b7e41b4aba27efb
Michał B <MadTiger2409@users.noreply.github.com>
2020-11-27 23:37:55 +0100

    New comments

--- CookieBook.Infrastructure/Parameters/Recipe/RecipesParameters.cs
+++ CookieBook.Infrastructure/Parameters/Recipe/RecipesParameters.cs
@@ -4,10 +4,33 @@ namespace CookieBook.Infrastructure.Parameters.Recipe
 {
     public class RecipesParameters : BaseParameters
     {
+        /// <summary>
+        /// Date of recipe creation
+        /// </summary>
         public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Indicates whether or not this meal from this recipe is lactose free. Skipped if null or empty
+        /// </summary>
+        /// <example>False</example>
         public bool? IsLactoseFree { get; set; }
+
+        /// <summary>
+        /// Indicates whether or not this meal from this recipe is gluten free. Skipped if null or empty
+        /// </summary>
+        /// <example>False</example>
         public bool? IsGlutenFree { get; set; }
+
+        /// <summary>
+        /// Indicates whether or not this meal from this recipe is good for vegans. Skipped if null or empty
+        /// </summary>
+        /// <example>False</example>
         public bool? IsVegan { get; set; }
+
+        /// <summary>
+        /// Indicates whether or not this meal from this recipe is good for vegetarians. Skipped if null or empty
+        /// </summary>
+        /// <example>True</example>
         public bool? IsVegetarian { get; set; }
     }
 }
